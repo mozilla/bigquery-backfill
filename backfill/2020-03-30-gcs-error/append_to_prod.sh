8366bdb0bca98f30314e8a07dc6995cb5a1d6d13
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-04-01 10:39:08 -0700

    Add script to append data to prod

---
+++ backfill/2020-03-30-gcs-error/append_to_prod.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PROJECT=moz-fx-data-backfill-31
+
+for dataset in $(bq ls -n 1000 --project_id=moz-fx-data-shared-prod | grep '_stable'); do
+    for table in $(bq ls -n 1000 --project_id=moz-fx-data-shared-prod "$dataset" | grep TABLE | awk '{print $1}'); do
+        echo "copying $dataset.$table from $PROJECT into moz-fx-data-shared-prod"
+        src="${PROJECT}:${dataset}.${table}"
+        dst="moz-fx-data-shared-prod:${dataset}.${table}"
+        bq cp "$src" "$dst" --no_clobber --append_table
+    done
+done
