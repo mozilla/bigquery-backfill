b0853a3a9f50ac003c3f1450dd9bd6722f9e2a96
Jeff Klukas <jeff@klukas.net>
2022-04-27 12:41:36 -0400

    Update table refs

--- backfill/2022-04-27-firefox-desktop-glean-rejected/02-gcp-ingestion-beam-decoder.sh
+++ backfill/2022-04-27-firefox-desktop-glean-rejected/02-gcp-ingestion-beam-decoder.sh
@@ -2,7 +2,7 @@
 set -exo pipefail
 
 # This script assumes it's being run from the ingestion-beam directory of the gcp-ingestion repo
-# with the code modified to convert any events[*].timestamp float values into integers.
+# with the code set to current contents of the main branch.
 
 PROJECT="moz-fx-data-backfill-10"
 JOB_NAME="firefox-desktop-backfill"
@@ -19,14 +19,14 @@ mvn \
       --geoCityFilter=gs://moz-fx-data-prod-dataflow-templates/cities15000.txt \
       --schemasLocation=gs://moz-fx-data-prod-dataflow/schemas/202204250316_17084144.tar.gz \
       --inputType=bigquery_table \
-      --input=\"$PROJECT:payload_bytes_error.structured\" \
+      --input=\"$PROJECT:payload_bytes_error.backfill\" \
       --bqReadMethod=storageapi \
       --outputType=bigquery \
       --bqWriteMethod=file_loads \
       --bqClusteringFields=submission_timestamp \
       --output=$PROJECT:\${document_namespace}_live.\${document_type}_v\${document_version} \
       --errorOutputType=bigquery \
-      --errorOutput=$PROJECT:payload_bytes_error.backfill \
+      --errorOutput=$PROJECT:payload_bytes_error.structured \
       --experiments=shuffle_mode=service \
       --region=us-central1 \
       --usePublicIps=false \
