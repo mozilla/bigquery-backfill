62f759cb70309385fcf845f40286bc328951a0fa
lucia-vargas-a <alvargasa@gmail.com>
2024-04-27 00:48:19 +0200

    Queries to backfill active_users_aggregates.

---
+++ backfill/2024-04-26-active_users_aggregates/README.md
@@ -0,0 +1,18 @@
+# Backfill active_users_aggregates for mobile apps from 2021-01-01
+ 
+- Implements [DENG-3543](https://mozilla-hub.atlassian.net/browse/DENG-3543).
+- This backfill is required after implementing [DENG-2989](https://mozilla-hub.atlassian.net/browse/DENG-2989).
+- GCP project used:  `moz-fx-data-shared-prod`
+
+## Context
+The definition of active_bits, dau, wau, mau has been moved to the upstream views clients_last_seen for Desktop and
+baseline_clients_last_seen for Mobile.
+Integrating this new definition to active_user_aggregates requires a complete backfill from 2021. However, a direct backfill would affect
+the metrics due to shredder's impact, which is avoided in this PR by querying `unified metrics` (not impacted by shredder) to backfill
+in temporary tables `backfills_staging_derived.<app_name>_active_users_aggregates_v3_<backfill_date>`. 
+These tables will replace the aggregate tables in production after data is validated by Data Science.
+
+- Step 1: Backfill `backfills_staging_derived.<app_name>_active_users_aggregates_v3_<backfill_date>` from 2021-01-01 using the corresponding `<app_name>_query.sql`
+- Step 2: Validate (DS & DE) that both the table created in step 1 and the production table `<app_name>_derived.active_users_aggregates_v3` are an exact match.
+- DE [validation query](https://console.cloud.google.com/bigquery?ws=!1m7!1m6!12m5!1m3!1smozdata!2sus-central1!3sb281e5ab-74f3-43c9-b26a-f6d8f4bcb42a!2e1) in BigQuery. Should run per app.
+- Step 3: Copy the table created in step 1 to table <app_name>_derived.active_users_aggregates_v3, replacing the existing one.
---
+++ backfill/2024-04-26-active_users_aggregates/desktop_query.sql
@@ -0,0 +1,82 @@
+WITH baseline AS (
+  SELECT
+    activity_segment AS segment,
+    attribution_medium,
+    attribution_source,
+    attribution_medium IS NOT NULL
+    OR attribution_source IS NOT NULL AS attributed,
+    city,
+    country,
+    um.distribution_id AS distribution_id,
+    um.first_seen_date AS first_seen_date,
+    is_default_browser,
+    normalized_channel AS channel,
+    normalized_os AS os,
+    normalized_os_version AS os_version,
+    os_version_major,
+    os_version_minor,
+    um.submission_date,
+    um.locale,
+    CAST(NULL AS STRING) AS adjust_network,
+    CAST(NULL AS STRING) AS install_source,
+    normalized_app_name as app_name,
+    days_since_seen,
+    ad_click,
+    organic_search_count,
+    search_count,
+    search_with_ads,
+    uri_count,
+    active_hours_sum,
+    um.app_version as app_version,
+    um.client_id,
+    isp,
+    durations
+  FROM
+    `moz-fx-data-shared-prod.telemetry_derived.unified_metrics_v1` AS um
+  WHERE
+    um.submission_date = '2021-01-01' AND um.submission_date <= CURRENT_DATE
+    AND normalized_app_name = 'Firefox Desktop'
+)
+SELECT
+  segment,
+  CASE
+    WHEN isp = 'BrowserStack'
+      THEN CONCAT(app_name, ' ', isp)
+    WHEN distribution_id = 'MozillaOnline'
+      THEN CONCAT(app_name, ' ', distribution_id)
+    ELSE app_name
+  END AS app_name,
+  app_version,
+  channel,
+  country,
+  city,
+  COALESCE(REGEXP_EXTRACT(locale, r'^(.+?)-'), locale, NULL) AS locale,
+  EXTRACT(YEAR FROM first_seen_date) AS first_seen_year,
+  os,
+  os_version,
+  os_version_major,
+  os_version_minor,
+  submission_date,
+  is_default_browser,
+  distribution_id,
+  attribution_source,
+  attribution_medium,
+  attributed,
+  COUNT(DISTINCT IF(days_since_seen = 0, client_id, NULL)) AS daily_users,
+  COUNT(DISTINCT IF(days_since_seen < 7, client_id, NULL)) AS weekly_users,
+  COUNT(DISTINCT client_id) AS monthly_users,
+  COUNT(
+    DISTINCT IF(days_since_seen = 0 AND active_hours_sum > 0 AND uri_count > 0, client_id, NULL)
+  ) AS dau,
+  COUNT(DISTINCT IF(days_since_seen < 7 AND active_hours_sum > 0 AND uri_count > 0, client_id, NULL)) AS wau,
+  COUNT(DISTINCT IF(active_hours_sum > 0 AND uri_count > 0, client_id, NULL)) AS mau,
+  SUM(ad_click) AS ad_clicks,
+  SUM(organic_search_count) AS organic_search_count,
+  SUM(search_count) AS search_count,
+  SUM(search_with_ads) AS search_with_ads,
+  SUM(uri_count) AS uri_count,
+  SUM(active_hours_sum) AS active_hours
+FROM
+  baseline
+GROUP BY
+  ALL
