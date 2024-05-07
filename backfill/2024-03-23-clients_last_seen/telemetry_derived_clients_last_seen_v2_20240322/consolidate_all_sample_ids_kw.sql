bf1fa01a3a46550339affcaa91367c31cdc5c23a
Katie Windau <153020235+kwindau@users.noreply.github.com>
2024-05-07 09:08:23 -0500

    Update consolidate_all_sample_ids_kw.sql

--- backfill/2024-03-23-clients_last_seen/telemetry_derived_clients_last_seen_v2_20240322/consolidate_all_sample_ids_kw.sql
+++ backfill/2024-03-23-clients_last_seen/telemetry_derived_clients_last_seen_v2_20240322/consolidate_all_sample_ids_kw.sql
@@ -23,3 +23,5 @@ bq cp --append_table=true moz-fx-data-shared-prod:backfills_staging_derived.tele
 bq cp --append_table=true moz-fx-data-shared-prod:backfills_staging_derived.telemetry_derived_clients_last_seen_v2_20230322_4 moz-fx-data-shared-prod:telemetry_derived.clients_last_seen_v2_20240507
 
 bq cp --append_table=true moz-fx-data-shared-prod:backfills_staging_derived.telemetry_derived_clients_last_seen_v2_20230322_5 moz-fx-data-shared-prod:telemetry_derived.clients_last_seen_v2_20240507
+
+bq cp --append_table=true moz-fx-data-shared-prod:backfills_staging_derived.telemetry_derived_clients_last_seen_v2_20230322_6 moz-fx-data-shared-prod:telemetry_derived.clients_last_seen_v2_20240507
